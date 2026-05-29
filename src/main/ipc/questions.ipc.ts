b9beb2c243ff608abb2d86ab42c229d1d2fb9647
Carlos Javier Gonzalez Bautista <carlos.gonzalezba@anahuac.mx>
2026-05-29 00:55:46 -0600

    Creación de los manejadores IPC

---
+++ src/main/ipc/questions.ipc.ts
@@ -0,0 +1,54 @@
+import { ipcMain } from 'electron';
+import { createCategory, getAllCategories } from '../db/repositories/categories.repository';
+import {
+  createQuestion,
+  getQuestionSummaries,
+  type CreateQuestionInput,
+} from '../db/repositories/questions.repository';
+
+function registerHandler(channel: string, listener: Parameters<typeof ipcMain.handle>[1]) {
+  ipcMain.removeHandler(channel);
+  ipcMain.handle(channel, listener);
+}
+
+function getErrorMessage(error: unknown) {
+  if (error instanceof Error) {
+    return error.message;
+  }
+
+  return 'Ocurrió un error inesperado.';
+}
+
+export function registerQuestionsIpc() {
+  registerHandler('questions:getCategories', () => {
+    try {
+      return getAllCategories();
+    } catch (error) {
+      throw new Error(getErrorMessage(error));
+    }
+  });
+
+  registerHandler('questions:createCategory', (_event, name: string) => {
+    try {
+      return createCategory(name);
+    } catch (error) {
+      throw new Error(getErrorMessage(error));
+    }
+  });
+
+  registerHandler('questions:createQuestion', (_event, question: CreateQuestionInput) => {
+    try {
+      return createQuestion(question);
+    } catch (error) {
+      throw new Error(getErrorMessage(error));
+    }
+  });
+
+  registerHandler('questions:getQuestionSummaries', () => {
+    try {
+      return getQuestionSummaries();
+    } catch (error) {
+      throw new Error(getErrorMessage(error));
+    }
+  });
+}
