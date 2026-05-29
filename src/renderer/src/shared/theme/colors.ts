f529ecc5f7b7d4f7a4c2e3f3f4e84c341c7eabd5
Carlos Javier Gonzalez Bautista <carlos.gonzalezba@anahuac.mx>
2026-05-29 00:08:08 -0600

    Se añadió el archivo de colores de uso global

---
+++ src/renderer/src/shared/theme/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  primary: '#0092DD',
+  secondary: '#005ca9',
+
+  textLight: '#ffffff',
+  textDark: '#1d1d1b',
+
+  red: '#e94362',
+  green: '#2fac66',
+  yellow: '#fdc533',
+
+  gray: '#C6C6C6',
+  pink: '#f088b6',
+  purple: '#954B97',
+
+  overlayDark: 'rgba(0, 0, 0, 0.65)',
+  transparent: 'transparent',
+} as const;
